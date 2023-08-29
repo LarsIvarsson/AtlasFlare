84c1b8305e162bab288fc65ea9407e1bfd2261c1
Zebseb <113332074+Zebseb@users.noreply.github.com>
2023-08-29 19:55:40 +0200

    Adding logic for register page

--- AtlasFlare/ClientApp/src/components/Routes/Register.jsx
+++ AtlasFlare/ClientApp/src/components/Routes/Register.jsx
@@ -2,34 +2,41 @@
 
 function Register() {
     const [user, setUser] = useState({
-        username: "",
-        password: ""
+        UserId: 0,
+        Username: "",
+        Password: ""
     });
 
     function handleSubmit(e) {
         e.preventDefault();
+        user.UserId = Math.floor(Math.random() * 1000);
 
+        let message = fetch("user", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user),
+        });
     }
 
     function handleUsername(e) {
-        setUser({ ...user, username: e.target.value });
+        setUser({ ...user, Username: e.target.value });
     }
 
     function handlePassword(e) {
-        setUser({ ...user, password: e.target.value });
+        setUser({ ...user, Password: e.target.value });
     }
 
     return <div>
         <h1>Register here please...</h1>
         <form onSubmit={handleSubmit}>
             <label htmlFor="name-input">Username</label>
-            <input id="name-input" type="text" onChange={handleUsername} value={user.username}></input>
+            <input id="name-input" type="text" onChange={handleUsername} value={user.Username}></input>
             <label htmlFor="password-input"></label>
-            <input id="password-input" type="password" onChange={handlePassword} value={user.password}></input>
+            <input id="password-input" type="password" onChange={handlePassword} value={user.Password}></input>
             <button type="submit">Enter</button>
         </form>
         <hr />
-        <h3>Current input: {user.username} {user.password}</h3>
+        <h3>Current input: {user.Username} {user.Password}</h3>
     </div>
 }
 
--- AtlasFlare/Controllers/UserController.cs
+++ AtlasFlare/Controllers/UserController.cs
@@ -2,10 +2,11 @@
 using AtlasFlare.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace AtlasFlare.Controllers
 {
-	[Route("User/[controller]")]
+	[Route("[controller]")]
 	[ApiController]
 	public class UserController : ControllerBase
 	{
@@ -15,57 +16,84 @@ namespace AtlasFlare.Controllers
 			this.context = context;
 		}
 
-		// Sign in user
-		[HttpGet("{username}")]
-		public async Task<bool> Get(string username, [FromQuery] string password, [FromQuery] string userType)
+
+		[HttpGet("{id}")]
+		public StudentModel Get(int id)
 		{
-			UserModel? userToLogin = new();
-			if (userType == "teacher")
-			{
-				userToLogin = await context.Teachers.FirstOrDefaultAsync(s => s.Username == username);
-
-				if (userToLogin != null)
-				{
-					if (userToLogin.Password == password)
-					{
-						return true;
-					}
-				}
-				return false;
-			}
-			else
-			{
-				userToLogin = await context.Students.FirstOrDefaultAsync(s => s.Username == username);
-
-				if (userToLogin != null)
-				{
-					if (userToLogin.Password == password)
-					{
-						return true;
-					}
-				}
-				return false;
-			}
+			StudentModel student = context.Students.FirstOrDefault(s => s.UserId == id);
+			return student;
 		}
+
+
+		// Sign in user
+		//[HttpGet("{username}")]
+		//public async Task<bool> Get(string username, [FromQuery] string password, [FromQuery] string userType)
+		//{
+		//	UserModel? userToLogin = new();
+		//	if (userType == "teacher")
+		//	{
+		//		userToLogin = await context.Teachers.FirstOrDefaultAsync(s => s.Username == username);
+
+		//		if (userToLogin != null)
+		//		{
+		//			if (userToLogin.Password == password)
+		//			{
+		//				return true;
+		//			}
+		//		}
+		//		return false;
+		//	}
+		//	else
+		//	{
+		//		userToLogin = await context.Students.FirstOrDefaultAsync(s => s.Username == username);
+
+		//		if (userToLogin != null)
+		//		{
+		//			if (userToLogin.Password == password)
+		//			{
+		//				return true;
+		//			}
+		//		}
+		//		return false;
+		//	}
+		//}
 
 		// Create new user
 		[HttpPost]
-		public async Task<IActionResult> Post([FromBody] string username, string password)
+		public async Task<IActionResult> Post([FromBody] string jsonUser)
 		{
-			try
+            StudentModel? newStudent = JsonConvert.DeserializeObject<StudentModel>(jsonUser);
+
+			if (newStudent != null)
 			{
-				await context.Students.AddAsync(new StudentModel()
-				{
-					Username = username,
-					Password = password
-				});
-				await context.SaveChangesAsync();
-				return Ok();
-			}
-			catch
-			{
-				return BadRequest();
-			}
+                var existingStudent = await context.Students.Where(s => s.Username == newStudent.Username).FirstOrDefaultAsync();
+
+                if (existingStudent == null)
+                {
+                    await context.Students.AddAsync(newStudent);
+					await context.SaveChangesAsync();
+
+                    return Ok(newStudent);
+                }
+            }
+
+			return BadRequest();
+
+//         try
+			//{
+			//	await context.Students.AddAsync(new StudentModel()
+			//	{
+			//		//Username = username,
+			//		//Password = password
+					
+			//	});
+			//	await context.SaveChangesAsync();
+			//	return Ok();
+			//}
+			//catch
+			//{
+			//	return BadRequest();
+			//}
 		}
 	}
 }
