f946cab5abf2f9fc02bf92afbbcb556b4ea95b63
OscarKristland <113332057+OscarKristland@users.noreply.github.com>
2023-08-30 11:08:10 +0200

    Login
    KOlla så att statuskoden är ok om det blir en ok login

--- AtlasFlare/ClientApp/src/components/Routes/Login.jsx
+++ AtlasFlare/ClientApp/src/components/Routes/Login.jsx
@@ -1,32 +1,46 @@
 import { useState } from 'react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { redirect } from '../../../../../../../../node_modules/react-router/dist/index';
 
 function Login() {
 
-    const [user, setUser] = useState({
-        username: "",
-        password: ""
+    const [student, setStudent] = useState({
+        Username: '',
+        Password: '',
     });
 
-    function handleFormInput(e) {
-        const { name, value } = e.target;
-        setUser(prevUser => ({
-            ...prevUser,
-            [name]: value
-        }));
+    function handleUsername(e) {
+        setStudent({ ...student, Username: e.target.value });
+    }
+
+    function handlePassword(e) {
+        setStudent({ ...student, Password: e.target.value });
     }
 
     function handleLogin() {
-        //const response = fetch("user/{id}?password={password}");
-        //if (response.ok) {
-        //    //Ok login
-        //    //ta användaren till quiz sidan
-        //}
-        //else {
-        //    //Error login
-        //}
+        console.log("Username:", student.Username);
+        console.log("Password:", student.Password);
+        const username = student.Username;
+        const password = student.Password;
 
+        const url = `user?username=${username}&password=${password}`;
+
+        try {
+            fetch(url).then(response => {
+                if (response.ok) {
+                    //Ok login
+                    //ta användaren till quiz sidan
+                    console.log("Successful login");
+                }
+                else {
+                    //Error login
+                }
+            })
+        }
+        catch (error) {
+
+        }
     }
 
     function handleSubmit(e) {
@@ -39,8 +53,8 @@ function Login() {
         <div>
             <h1>Login</h1>
             <form onSubmit={handleSubmit}>
-                <input placeholder='Username' onChange={handleFormInput} type="text" name="username"></input>
-                <input placeholder='Password' onChange={handleFormInput} type="password" name="password"></input>
+                <input id="name-input" placeholder='Username' onChange={handleUsername} type="text" value={student.Username}></input>
+                <input id="password-input" placeholder='Password' onChange={handlePassword} type="password" value={student.Password}></input>
 
                 <button type="submit">Login</button>
             </form>
