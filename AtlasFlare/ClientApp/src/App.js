a2aa2481277bd5d5b32a58cfd99576c20fb0b469
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-08-30 14:23:42 +0200

    Quiz Links and component

--- AtlasFlare/ClientApp/src/App.js
+++ AtlasFlare/ClientApp/src/App.js
@@ -4,6 +4,7 @@ import Menu from './components/Routes/Menu'
 import Home from './components/Routes/Home';
 import Login from './components/Routes/Login';
 import Register from './components/Routes/Register';
+import Quiz from './components/Routes/Quiz';
 import './custom.css';
 import './styles/Home.css';
 
@@ -15,6 +16,7 @@ function App() {
             <Route path="/menu" element={<Menu />} />          
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register /> } />
+            <Route path="/quiz" element={<Quiz /> } />
             </Routes>
     );
 }
