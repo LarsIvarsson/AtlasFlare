8562dc6a8a69bb701578710374c25810541daea0
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-08-30 11:27:46 +0200

    create menu page
    menu page no layout, removing test controller

--- AtlasFlare/ClientApp/src/App.js
+++ AtlasFlare/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './components/Routes/Home'
-import Names from './components/Routes/Names'
+import Menu from './components/Routes/Menu'
 import "../src/styles/Home.css";
 /*import Login from './components/Routes/Login';*/
 import Register from './components/Routes/Register';
@@ -14,7 +14,7 @@ function App() {
     return (
             <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/names" element={<Names />} />
+            <Route path="/menu" element={<Menu />} />
             {/*<Route path="/login" element={<Login />} />*/}
             <Route path="/register" element={<Register /> } />
             </Routes>
