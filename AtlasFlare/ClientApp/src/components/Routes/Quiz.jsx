ef1cd2295af72881c925c894893e21b2dae5c18f
OscarKristland <113332057+OscarKristland@users.noreply.github.com>
2023-09-04 14:39:28 +0200

    idéer quiz

--- AtlasFlare/ClientApp/src/components/Routes/Quiz.jsx
+++ AtlasFlare/ClientApp/src/components/Routes/Quiz.jsx
@@ -1,6 +1,18 @@
 ﻿import React from 'react';
 
 function Quiz() {
+
+    let wrongAnswer;
+    let correctAnswer;
+    let totalQuestions;
+
+
+    //Dessa kan sedan fyllas med info från fetch, exempelvis med hur många frågor som kommer ställas.
+    //wrong/correct svar sparas här och kan sedan tas vidare och se resultatet av hela quizzen????
+    //
+
+
+
     return (
         <div>
             <div className="content">
