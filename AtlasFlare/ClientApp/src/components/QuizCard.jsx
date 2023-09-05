dda9bc0b553978c60fb069949ec64a133cdb57a5
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-09-05 15:07:42 +0200

    adding quizcard
    basic card code deployed

---
+++ AtlasFlare/ClientApp/src/components/QuizCard.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import '../styles/QuizCard.css';
+
+function QuizCard(props) {
+    return (
+        <div>
+            <h1>{props.flag.countryName}</h1>
+            <img src={props.flag.imageUrl} alt={props.flag.countryName}/>
+        </div>
+    )
+}
+
+export default QuizCard;
