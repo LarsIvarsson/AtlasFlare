a4cb8c47702e325c371a8b0b22119afcb499f8b1
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-09-05 15:22:26 +0200

    quizcard
    small correction changes

--- AtlasFlare/ClientApp/src/components/QuizCard.jsx
+++ AtlasFlare/ClientApp/src/components/QuizCard.jsx
@@ -4,8 +4,12 @@ import '../styles/QuizCard.css';
 function QuizCard(props) {
     return (
         <div>
-            <h1>{props.flag.countryName}</h1>
-            <img src={props.flag.imageUrl} alt={props.flag.countryName}/>
+            <div className="content">
+                <div className="card">
+                    <h1>{props.flag.countryName}</h1>
+                    <img className="quiz-flag" src={props.flag.imageUrl} alt={props.flag.countryName} />
+                </div>
+            </div>
         </div>
     )
 }
