91a9ca51fb2b079eaaca1d6202b97bb633c4d833
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-09-06 10:08:33 +0200

    quiz style

--- AtlasFlare/ClientApp/src/components/QuizCard.jsx
+++ AtlasFlare/ClientApp/src/components/QuizCard.jsx
@@ -9,12 +9,14 @@ function QuizCard({ flags }) {
     }, [flags])
 
     function handleNextClick() {
+        // lägg till kontroll av svar
         if (currentIndex < flags.length - 1) {
             setCurrentIndex(currentIndex + 1);
         }
     }
 
     function handlePrevClick() {
+        // kanske inte ska vara med?
         if (currentIndex > 0) {
             setCurrentIndex(currentIndex - 1);
         }
@@ -30,15 +32,14 @@ function QuizCard({ flags }) {
         <div>
             <div className="quiz-content">
                 <div className="quiz-card">
-                    <img className="quiz-flag" src={flags[currentIndex].imageUrl} alt={currentFlag.countryName} />
-                    <h3>{currentFlag.countryName}</h3>
+                    <img className="quiz-flag" src={currentFlag.imageUrl} alt={currentFlag.countryName} />
+                    <h3>{currentIndex + 1} / {flags.length }: {currentFlag.countryName}</h3>
                     <button onClick={handlePrevClick} disabled={currentIndex === 0}>
                         Previous
                     </button>
                     <button onClick={handleNextClick} disabled={currentIndex === flags.length - 1}>
                         Next
                     </button>
-
                 </div>
             </div>
         </div>
