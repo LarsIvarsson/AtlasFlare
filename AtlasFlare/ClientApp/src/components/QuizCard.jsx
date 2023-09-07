b6925e249b83459f5738f6aac9c8dbc14c44f375
OscarKristland <113332057+OscarKristland@users.noreply.github.com>
2023-09-07 14:37:36 +0200

    Försök Randomisering
    Försökt få randomisering att fungera, den vill i nuläget randomisera men den kan inte kolla om ett tidigare land har varit valt.

--- AtlasFlare/ClientApp/src/components/QuizCard.jsx
+++ AtlasFlare/ClientApp/src/components/QuizCard.jsx
@@ -1,76 +1,190 @@
-﻿import React, { useEffect, useState } from 'react';
+﻿//import React, { useEffect, useState } from 'react';
+//import '../styles/QuizCard.css';
+
+//function QuizCard({ flags, continent }) {
+//    //Behövs för att ladda första flaggan
+//    const allCountries = flags;
+//    const [correctCountry, setCorrectCountry] = useState(null);
+//    //Till hit
+//    const [currentIndex, setCurrentIndex] = useState(0);
+//    const [random1, setRandom1] = useState(0);
+//    const [random2, setRandom2] = useState(0);
+//    const [random3, setRandom3] = useState(0);
+//    const currentFlag = flags[currentIndex];
+//    const altFlag1 = flags[random1];
+//    const altFlag2 = flags[random2];
+//    const altFlag3 = flags[random3];
+//    const currentContinent = continent.toUpperCase();
+//    const [isAltFlags, setIsAltFlags] = useState(false);
+//    const [altArray, setAltArray] = useState();
+//    const [altOneSet, setAltOneSet] = useState(false);
+
+//    //Randomisera första flaggan
+//    useEffect(() => {
+//        const randomSelectedCountry = getRandomCountry(allCountries);
+//        setCorrectCountry(randomSelectedCountry);
+//    }, [allCountries]);
+
+//    useEffect(() => {
+//        setRandom1(getRandom(flags.length - 1));
+//        setRandom2(getRandom(flags.length - 1));
+//        setRandom3(getRandom(flags.length - 1));
+//    }, [flags, currentIndex])
+
+//    useEffect(() => {
+//        if (altFlag1 && altFlag2 && altFlag3 && currentFlag) {
+//            setIsAltFlags(true);
+//            setAltArray([altFlag1.countryName, altFlag2.countryName, altFlag3.countryName, currentFlag.countryName]);
+//        }
+//    }, [altFlag1, altFlag2, altFlag3, currentFlag])
+
+//    //Randomisera vilken flagga som ska visas först
+//    const getRandomCountry = (countries) => {
+//        const randomIndex = Math.floor(Math.random() * countries.length);
+//        return countries[randomIndex];
+//    };
+
+//    function randomNumber(mn, mx) {
+//        return Math.random() * (mx - mn) + mn;
+//    }
+
+//    function getRandomFlag() {
+///*        let altArray = [altFlag1.countryName, altFlag2.countryName, altFlag3.countryName, currentFlag.countryName];*/
+///*        const flagIndex = (Math.floor(randomNumber(0, 4)));*/
+//        /*        const flagName = altArray[flagIndex];*/
+//        const flagName = altArray[Math.floor(Math.random() * altArray.length)];
+
+//        return flagName;
+//    }
+
+//    function getAltOne() {
+//        //Kan fixa getAltOne, getAltTwo osv och uppdatera arrayen efter varje hämtning samt "setAltOneSet" = true osv.
+//        //Kan displaya olika beroende på altOneSet, altTwoSet osv.
+//        const flagName = altArray[Math.floor(Math.random() * altArray.length)];
+//        let updatedArray = altArray.filter(f => f !== flagName);
+//        console.log(updatedArray)
+
+//        return flagName;
+//    }
+
+//    function getRandom(max) {
+//        //Fixa så att man inte får samma siffra två gånger
+//        return Math.floor(Math.random() * max);
+//    }
+
+//    function handleNextClick() {
+//        // lägg till kontroll av svar
+//        if (currentIndex < flags.length - 1) {
+//            setCurrentIndex(currentIndex + 1);
+//        }
+//    }
+
+//    function handlePrevClick() {
+//        // kanske inte ska vara med?
+//        if (currentIndex > 0) {
+//            setCurrentIndex(currentIndex - 1);
+//        }
+//    }
+
+//    if (!currentFlag || !altFlag1 || !altFlag2 || !altFlag3) {
+//        return <div>Loading...</div>;
+//    }
+
+//    return (
+//        <div>
+//            <div className="quiz-content">
+//                <div className="info-card">
+//                    <p id="continet-name">{currentContinent}</p>
+//                    <p id="progress">{currentIndex + 1} / {flags.length}</p>
+//                </div>
+//                <div className="quiz-card">
+//                    <div className="country-container">
+//                        <div className="flag-container">
+//                            <img className="quiz-flag" src={correctCountry.imageUrl} alt={currentFlag.countryName} />
+//                        </div>
+//                        <div className="btn-container">
+//                            <button id="btn-prev" onClick={handlePrevClick} disabled={currentIndex === 0}>
+//                                PREVIOUS
+//                            </button>
+//                            <button id="btn-next" onClick={handleNextClick} disabled={currentIndex === flags.length - 1}>
+//                                NEXT
+//                            </button>
+//                        </div>
+//                    </div>
+//                    {isAltFlags ?
+//                        (<div className="answer-container">
+//                            <button className="btn-answer">{getRandomFlag()}</button>
+//                            <button className="btn-answer">{getRandomFlag()}</button>
+//                            <button className="btn-answer">{getRandomFlag()}</button>
+//                            <button className="btn-answer">{getRandomFlag()}</button>
+//                        </div>) :
+
+//                        (<div className="answer-container">
+//                        <button className="btn-answer">""</button>
+//                        <button className="btn-answer">""</button>
+//                        <button className="btn-answer">""</button>
+//                        <button className="btn-answer">""</button>
+//                    </div>)}
+
+//                </div>
+//            </div>
+//        </div>
+//    )
+//}
+
+//export default QuizCard;
+
+import React, { useEffect, useState } from 'react';
 import '../styles/QuizCard.css';
 
 function QuizCard({ flags, continent }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [random1, setRandom1] = useState(0);
-    const [random2, setRandom2] = useState(0);
-    const [random3, setRandom3] = useState(0);
-    const currentFlag = flags[currentIndex];
-    const altFlag1 = flags[random1];
-    const altFlag2 = flags[random2];
-    const altFlag3 = flags[random3];
+    const allCountries = flags;
     const currentContinent = continent.toUpperCase();
-    const [isAltFlags, setIsAltFlags] = useState(false);
-    const [altArray, setAltArray] = useState();
-    const [altOneSet, setAltOneSet] = useState(false);
+    const [correctCountry, setCorrectCountry] = useState(null);
+    const [previousCountry, setPreviousCountry] = useState(null);
+
+    //const getRandomCountry = (countries) => {
+    //    const randomIndex = Math.floor(Math.random() * countries.length);
+    //    return countries[randomIndex];
+    //};
+    const getRandomCountry = (countries, previousCountry) => {
+        let randomIndex;
+        let selectedCountry;
+
+        // Loopa tills ett unikt land har valts
+        do {
+            randomIndex = Math.floor(Math.random() * countries.length);
+            selectedCountry = countries[randomIndex];
+        } while (selectedCountry === previousCountry && previousCountry !== undefined);
+
+        return selectedCountry;
+    };
 
     useEffect(() => {
-        setRandom1(getRandom(flags.length - 1));
-        setRandom2(getRandom(flags.length - 1));
-        setRandom3(getRandom(flags.length - 1));
-    }, [flags, currentIndex])
+        const randomSelectedCountry = getRandomCountry(allCountries);
+        setCorrectCountry((prevCorrectCountry) => {
+            setPreviousCountry(prevCorrectCountry);
+            return randomSelectedCountry;
+        });
+    }, [allCountries]);
 
     useEffect(() => {
-        if (altFlag1 && altFlag2 && altFlag3 && currentFlag) {
-            setIsAltFlags(true);
-            setAltArray([altFlag1.countryName, altFlag2.countryName, altFlag3.countryName, currentFlag.countryName]);
-        }
-    }, [altFlag1, altFlag2, altFlag3, currentFlag])
-
-    function randomNumber(mn, mx) {
-        return Math.random() * (mx - mn) + mn;
-    }
-
-    function getRandomFlag() {
-/*        let altArray = [altFlag1.countryName, altFlag2.countryName, altFlag3.countryName, currentFlag.countryName];*/
-/*        const flagIndex = (Math.floor(randomNumber(0, 4)));*/
-        /*        const flagName = altArray[flagIndex];*/
-        const flagName = altArray[Math.floor(Math.random() * altArray.length)];
-
-        return flagName;
-    }
-
-    function getAltOne() {
-        //Kan fixa getAltOne, getAltTwo osv och uppdatera arrayen efter varje hämtning samt "setAltOneSet" = true osv.
-        //Kan displaya olika beroende på altOneSet, altTwoSet osv.
-        const flagName = altArray[Math.floor(Math.random() * altArray.length)];
-        let updatedArray = altArray.filter(f => f !== flagName);
-        console.log(updatedArray)
-
-        return flagName;
-    }
-
-    function getRandom(max) {
-        //Fixa så att man inte får samma siffra två gånger
-        return Math.floor(Math.random() * max);
-    }
+        console.log("previousCountry:", previousCountry);
+    }, [previousCountry]);
 
     function handleNextClick() {
-        // lägg till kontroll av svar
-        if (currentIndex < flags.length - 1) {
-            setCurrentIndex(currentIndex + 1);
-        }
+        //reset correctCountry variable
+        //Get a new country from the allCountries variable
+        //Put the new country in the correctCountry variable
+        
+        const randomSelectedCountry = getRandomCountry(allCountries);
+        setPreviousCountry(correctCountry);
+        /*console.log("previousCountry:", previousCountry);*/
+        setCorrectCountry(randomSelectedCountry);
     }
 
-    function handlePrevClick() {
-        // kanske inte ska vara med?
-        if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-        }
-    }
-
-    if (!currentFlag || !altFlag1 || !altFlag2 || !altFlag3) {
+    if (!correctCountry) {
+        console.log("correctCountry är inte laddad");
         return <div>Loading...</div>;
     }
 
@@ -79,37 +193,18 @@ function QuizCard({ flags, continent }) {
             <div className="quiz-content">
                 <div className="info-card">
                     <p id="continet-name">{currentContinent}</p>
-                    <p id="progress">{currentIndex + 1} / {flags.length}</p>
                 </div>
                 <div className="quiz-card">
                     <div className="country-container">
                         <div className="flag-container">
-                            <img className="quiz-flag" src={currentFlag.imageUrl} alt={currentFlag.countryName} />
+                            <img className="quiz-flag" src={correctCountry.imageUrl} alt={correctCountry.countryName} />
                         </div>
                         <div className="btn-container">
-                            <button id="btn-prev" onClick={handlePrevClick} disabled={currentIndex === 0}>
-                                PREVIOUS
-                            </button>
-                            <button id="btn-next" onClick={handleNextClick} disabled={currentIndex === flags.length - 1}>
+                            <button id="btn-next" onClick={handleNextClick}>
                                 NEXT
                             </button>
                         </div>
                     </div>
-                    {isAltFlags ?
-                        (<div className="answer-container">
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                        </div>) :
-
-                        (<div className="answer-container">
-                        <button className="btn-answer">""</button>
-                        <button className="btn-answer">""</button>
-                        <button className="btn-answer">""</button>
-                        <button className="btn-answer">""</button>
-                    </div>)}
-
                 </div>
             </div>
         </div>
