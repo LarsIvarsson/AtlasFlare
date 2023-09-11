9959e03a845c18da2556a0a1ee0c83add260952a
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-09-11 10:59:08 +0200

    lots of quiz logic

--- AtlasFlare/ClientApp/src/components/QuizCard.jsx
+++ AtlasFlare/ClientApp/src/components/QuizCard.jsx
@@ -1,61 +1,11 @@
-﻿import React, { useEffect, useState } from 'react';
+﻿import React from 'react';
 import '../styles/QuizCard.css';
 
-function QuizCard({ flags, continent }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [random1, setRandom1] = useState(0);
-    const [random2, setRandom2] = useState(0);
-    const [random3, setRandom3] = useState(0);
+function QuizCard({ flags, continent, currentIndex, altArray, setCurrentIndex })
+{
     const currentFlag = flags[currentIndex];
-    const altFlag1 = flags[random1];
-    const altFlag2 = flags[random2];
-    const altFlag3 = flags[random3];
     const currentContinent = continent.toUpperCase();
-    const [isAltFlags, setIsAltFlags] = useState(false);
-    const [altArray, setAltArray] = useState();
-    const [altOneSet, setAltOneSet] = useState(false);
-
-    useEffect(() => {
-        setRandom1(getRandom(flags.length - 1));
-        setRandom2(getRandom(flags.length - 1));
-        setRandom3(getRandom(flags.length - 1));
-    }, [flags, currentIndex])
-
-    useEffect(() => {
-        if (altFlag1 && altFlag2 && altFlag3 && currentFlag) {
-            setIsAltFlags(true);
-            setAltArray([altFlag1.countryName, altFlag2.countryName, altFlag3.countryName, currentFlag.countryName]);
-        }
-    }, [altFlag1, altFlag2, altFlag3, currentFlag])
-
-    function randomNumber(mn, mx) {
-        return Math.random() * (mx - mn) + mn;
-    }
-
-    function getRandomFlag() {
-/*        let altArray = [altFlag1.countryName, altFlag2.countryName, altFlag3.countryName, currentFlag.countryName];*/
-/*        const flagIndex = (Math.floor(randomNumber(0, 4)));*/
-        /*        const flagName = altArray[flagIndex];*/
-        const flagName = altArray[Math.floor(Math.random() * altArray.length)];
-
-        return flagName;
-    }
-
-    function getAltOne() {
-        //Kan fixa getAltOne, getAltTwo osv och uppdatera arrayen efter varje hämtning samt "setAltOneSet" = true osv.
-        //Kan displaya olika beroende på altOneSet, altTwoSet osv.
-        const flagName = altArray[Math.floor(Math.random() * altArray.length)];
-        let updatedArray = altArray.filter(f => f !== flagName);
-        console.log(updatedArray)
-
-        return flagName;
-    }
-
-    function getRandom(max) {
-        //Fixa så att man inte får samma siffra två gånger
-        return Math.floor(Math.random() * max);
-    }
-
+   
     function handleNextClick() {
         // lägg till kontroll av svar
         if (currentIndex < flags.length - 1) {
@@ -63,57 +13,35 @@ function QuizCard({ flags, continent }) {
         }
     }
 
-    function handlePrevClick() {
-        // kanske inte ska vara med?
-        if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-        }
-    }
+    if (!flags || !currentFlag || !altArray) {
+        return <div>Loader...</div>
+    }    
 
-    if (!currentFlag || !altFlag1 || !altFlag2 || !altFlag3) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div>
-            <div className="quiz-content">
-                <div className="info-card">
-                    <p id="continet-name">{currentContinent}</p>
-                    <p id="progress">{currentIndex + 1} / {flags.length}</p>
-                </div>
-                <div className="quiz-card">
-                    <div className="country-container">
-                        <div className="flag-container">
-                            <img className="quiz-flag" src={currentFlag.imageUrl} alt={currentFlag.countryName} />
-                        </div>
-                        <div className="btn-container">
-                            <button id="btn-prev" onClick={handlePrevClick} disabled={currentIndex === 0}>
-                                PREVIOUS
-                            </button>
-                            <button id="btn-next" onClick={handleNextClick} disabled={currentIndex === flags.length - 1}>
-                                NEXT
-                            </button>
-                        </div>
+    return <div>
+        <div className="quiz-content">
+            <div className="info-card">
+                <p id="continet-name">{currentContinent}</p>
+                <p id="progress">{currentIndex + 1} / {flags.length}</p>
+            </div>
+            <div className="quiz-card">
+                <div className="country-container">
+                    <div className="flag-container">
+                        <img className="quiz-flag" src={currentFlag.imageUrl} alt={currentFlag.countryName} />
                     </div>
-                    {isAltFlags ?
-                        (<div className="answer-container">
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                            <button className="btn-answer">{getRandomFlag()}</button>
-                        </div>) :
-
-                        (<div className="answer-container">
-                        <button className="btn-answer">""</button>
-                        <button className="btn-answer">""</button>
-                        <button className="btn-answer">""</button>
-                        <button className="btn-answer">""</button>
-                    </div>)}
-
+                    <div className="btn-container">                        
+                        <button id="btn-next" onClick={handleNextClick} disabled={currentIndex === flags.length - 1}>
+                            NEXT
+                        </button>
+                    </div>
                 </div>
+                <div className="answer-container">
+                    {altArray.map((f) => (
+                        <button key={f.flagId } className="btn-answer">{f.countryName}</button>
+                    ))}
+                </div>
+                
             </div>
         </div>
-    )
+    </div>
 }
-
 export default QuizCard;
