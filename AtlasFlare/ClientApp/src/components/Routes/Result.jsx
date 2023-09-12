2c01eb68e6ed427e5c2a58850abf8efbb0b50a97
Beachbay <113332198+Beachbay@users.noreply.github.com>
2023-09-12 13:38:22 +0200

    ui an logic for the result page

--- AtlasFlare/ClientApp/src/components/Routes/Result.jsx
+++ AtlasFlare/ClientApp/src/components/Routes/Result.jsx
@@ -1,11 +1,21 @@
 ﻿import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Result() {
     const [resultsArray, setResultsArray] = useState([]);
     const [arrays, setArrays] = useState(false);
     const { difficultyArray } = useLocation().state;
     let i = 0;
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const handleScroll = event => {
+        setScrollTop(event.currentTarget.scrollTop);
+    };
 
     useEffect(() => {
         getResultsArray();
@@ -22,30 +32,45 @@ function Result() {
         <div>
             <div className="quiz-content">
                 <div className="info-card">
-                    <p id="continent-name"> New high score: </p>
-                    <p id="progress">Current high score: </p>
+                    <p id="continent-name"> RESULT: /{difficultyArray.length} </p>
+                    
                 </div>
                 <div className="quiz-card">
-                    <div className="result-container">
+                    <div className="result-container" style={{
+                        border: 'none',
+                        width: '800px',
+                        height: '350px',
+                        overflow: 'scroll',
+                    }} onScroll={handleScroll}>
                         {arrays ? (<div>
                             {difficultyArray.map((f, index) => {
                                 if (difficultyArray[index].countryName.toString() === resultsArray[index]) {
+                                   
                                     return <div key={index} className="result-card">
-                                        <img className="result-flag" src={f.imageUrl} alt="Hehe" /> <div>{f.countryName}</div> <h3 id="statement">CORRECT</h3> 
-                                        
+                                        <div className="img-name">
+                                            <img className="result-flag" src={f.imageUrl} alt="Hehe" />
+                                            <div id="c-name">{f.countryName}</div>
+                                        </div>
+
+                                        <FontAwesomeIcon icon={faCheck} id="v-mark" />
                                     </div>
                                 }
                                 else {
                                     return <div key={index} className="result-card">
-                                        <img className="result-flag" src={f.imageUrl} alt="Hehe" />  <div>{f.countryName}</div> <h3 id="statement">WRONG</h3> 
+                                        <div className="img-name">
+                                            <img className="result-flag" src={f.imageUrl} alt="Hehe" />
+                                            <div>{f.countryName}</div>
+                                        </div>
                                         
+                                        <FontAwesomeIcon icon={faXmark} id="x-mark" />
+
                                     </div>
                                 }
                             })}
-                        </div>) : (<div></div>) }
+                        </div>) : (<div></div>)}
 
                     </div>
-                    
+
                 </div>
             </div>
         </div>
