e1bcaf7e4cab73bc2fe484120500d0cc4cc50847
OscarKristland <113332057+OscarKristland@users.noreply.github.com>
2023-09-14 12:49:56 +0200

    Välkomst text på quiz. NOT FINISHED
    Lagt till en Välkomst text på quiz. Gjort lite styling till den skiten. Inte färdig

--- AtlasFlare/ClientApp/src/components/Routes/Menu.jsx
+++ AtlasFlare/ClientApp/src/components/Routes/Menu.jsx
@@ -4,10 +4,18 @@ import "../../styles/Menu.css";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Menu() {
+function Menu(props) {
+
+    console.log(props.username);
 
     return (
-        <div>            
+        <div>
+            <div className="greeting-username">
+                <h3>Welcome {props.username}!</h3>
+            </div>
+            <div className="greeting-container">
+                <h1>Please chooze quez, soviet style.</h1>
+            </div>
             <div className="card-container">
                 <div className="menu-card">
                     <h2 className="h2-header">NORTH AMERICA</h2>
